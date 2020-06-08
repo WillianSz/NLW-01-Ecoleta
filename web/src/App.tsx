5c7e7b4df818e82db70a2041c76bc46307acffb6
Willian Souza <willianscript@gmail.com>
2020-06-07 21:14:11 -0300

    Aula 3 Configurando API NLW!

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import Routes from './routes'
+// eslint-disable-next-line
+import CreatePoint from './pages/CreatePoint'
+
+function App() {
+  return (
+      <Routes />
+  );
+}
+
+export default App;
